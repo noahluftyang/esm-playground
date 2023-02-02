73c119c6e6d65f985d847e4b109c18f27cf69aa4
noahluftyang <euihyun.yang.9x@gmail.com>
2023-02-02 18:58:08 +0900

    feat(with-next): setup some lib

--- examples/with-next/next.config.js
+++ examples/with-next/next.config.js
@@ -1,10 +1,17 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
 import withMDX from "@next/mdx";
 
+const shouldAnalyze = (process.env.ANALYZE = "true");
+
 /** @type {import('next').NextConfig} */
-export default withMDX()({
-  pageExtensions: ["mdx", "ts", "tsx"],
-  experimental: {
-    appDir: true,
-    mdxRs: true,
-  },
-});
+export default withBundleAnalyzer({
+  enabled: shouldAnalyze,
+})(
+  withMDX()({
+    pageExtensions: ["mdx", "ts", "tsx"],
+    experimental: {
+      appDir: true,
+      mdxRs: true,
+    },
+  })
+);
