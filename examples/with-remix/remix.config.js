21668540ea025c86a671895ebb0a12f3b9a46592
noahluftyang <euihyun.yang.9x@gmail.com>
2023-02-02 20:26:54 +0900

    feat(with-remix): add netlify adapter

--- examples/with-remix/remix.config.js
+++ examples/with-remix/remix.config.js
@@ -1,7 +1,7 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
-module.exports = {
+export default {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
 };
