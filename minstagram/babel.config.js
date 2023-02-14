42e2b699163f83d85f2196b1bc0cf97b1e0f6f78
noahluftyang <euihyun.yang.9x@gmail.com>
2023-02-14 21:18:31 +0900

    fix(minstagram): ongoing expo-router

--- minstagram/babel.config.js
+++ minstagram/babel.config.js
@@ -3,6 +3,6 @@ module.exports = function (api) {
 
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    plugins: ["expo-router/babel", "nativewind/babel"],
   };
 };
