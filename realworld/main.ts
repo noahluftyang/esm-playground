2e496683adc18c849c68808dc160f85a3bba9092
noahluftyang <euihyun.yang.9x@gmail.com>
2023-03-01 16:49:16 +0900

    fix(realworld): migrate user related apis

--- realworld/main.ts
+++ realworld/main.ts
@@ -15,6 +15,36 @@ export default {
   fetch(request: Request, env: Env) {
     const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
 
+    // login
+    router.post("/users/login", async (request) => {
+      const body = await request.json();
+
+      const BodySchema = z.object({
+        user: z.object({
+          email: z.string().email(),
+          password: z.string(),
+        }),
+      });
+      const { user: userBody } = BodySchema.parse(body);
+
+      const { error, session, user } = await supabase.auth.signIn(userBody);
+
+      if (error != null) {
+        return new Response("Invalid login credentials.", { status: 401 });
+      }
+
+      return new JsonResponse({
+        user: {
+          bio: null,
+          email: user?.email,
+          image: null,
+          token: session?.access_token,
+          username: null,
+        },
+      });
+    });
+
+    // signup
     router.post("/users", async (request) => {
       const body = await request.json();
 
@@ -25,16 +55,24 @@ export default {
           username: z.string(),
         }),
       });
-
       const { user: userBody } = BodySchema.parse(body);
 
-      const { error, session, user } = await supabase.auth.signUp({
-        email: userBody.email,
-        password: userBody.password,
-      });
+      const { error, session, user } = await supabase.auth.signUp(
+        {
+          email: userBody.email,
+          password: userBody.password,
+        },
+        {
+          data: {
+            username: userBody.username,
+          },
+        }
+      );
 
       if (error != null) {
-        return new Response("Invalid login credentials.", { status: 401 });
+        return new Response("Invalid login credentials.", {
+          status: 401,
+        });
       }
 
       return new JsonResponse({
@@ -43,7 +81,73 @@ export default {
           email: user!.email,
           image: null,
           token: session!.access_token,
-          username: null,
+          username: user?.user_metadata.username,
+        },
+      });
+    });
+
+    // get current user
+    router.get("/user", async (request) => {
+      const [_prefix, token] = (
+        request.headers.get("Authorization") ?? ""
+      ).split("Token ");
+
+      const { error, user } = await supabase.auth.api.getUser(token);
+
+      if (error != null) {
+        return new Response(error.message, { status: error.status });
+      }
+
+      return new JsonResponse({
+        user: {
+          bio: null,
+          email: user?.email,
+          image: null,
+          token,
+          username: user?.user_metadata.username,
+        },
+      });
+    });
+
+    // update user
+    router.put("/user", async (request) => {
+      const [_prefix, token] = (
+        request.headers.get("Authorization") ?? ""
+      ).split("Token ");
+
+      const body = await request.json();
+
+      const BodySchema = z.object({
+        userBody: z.object({
+          bio: z.string(),
+          email: z.string().email(),
+          image: z.string().url(),
+          password: z.string(),
+          username: z.string(),
+        }),
+      });
+      const { userBody } = BodySchema.parse(body);
+
+      const { error, user } = await supabase.auth.update({
+        password: userBody.password,
+        data: {
+          bio: userBody.bio,
+          image: userBody.image,
+          username: userBody.username,
+        },
+      });
+
+      if (error != null) {
+        return new Response(error.message, { status: error.status });
+      }
+
+      return new JsonResponse({
+        user: {
+          bio: user?.user_metadata.bio,
+          email: user?.email,
+          image: user?.user_metadata.image,
+          token,
+          username: user?.user_metadata.username,
         },
       });
     });
