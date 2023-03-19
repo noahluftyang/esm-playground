20fa44ddabbba1322d60e878e1ba7831e206d311
noahluftyang <euihyun.yang.9x@gmail.com>
2023-03-19 17:32:31 +0900

    fix(blog): fix build error

--- blog/src/environments.ts
+++ blog/src/environments.ts
@@ -1,12 +1,14 @@
 import getConfig from "next/config";
 import z from "zod";
 
-const PublicRuntimeConfigSchema = z.object({
-  GOOGLE_ANALYTICS_ID: z.string(),
-});
+const PublicRuntimeConfigSchema = z
+  .object({
+    GOOGLE_ANALYTICS_ID: z.string(),
+  })
+  .partial();
 
 const config = getConfig();
 
 export const environments = PublicRuntimeConfigSchema.parse(
-  config.publicRuntimeConfig
+  config?.publicRuntimeConfig ?? {}
 );
