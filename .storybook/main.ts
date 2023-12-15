245c4f94d50213418a5a1028d9b07cadda6fb074
noahluftyang <euihyun.yang.9x@gmail.com>
2023-12-15 20:24:41 +0900

    version up dev dependencies

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,10 +1,11 @@
+import { dirname, join } from 'path';
 import { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: [],
-  framework: '@storybook/react-vite',
+  framework: getAbsolutePath('@storybook/react-vite'),
   core: {
-    builder: '@storybook/builder-vite',
+    builder: getAbsolutePath('@storybook/builder-vite'),
   },
   refs: {
     components: {
@@ -12,6 +13,13 @@ const config: StorybookConfig = {
       url: 'https://noahverse-components-storybook.netlify.app',
     },
   },
+  docs: {
+    autodocs: true,
+  },
 };
 
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
